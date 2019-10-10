3b313adc0320d40a9c74bb25cb84b46813c00c74
Ilkka <ilkka.salmenius@superapp.fi>
2019-10-10 14:26:47 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TravelMap class="travel-map"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TravelMap from './components/TravelMap.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TravelMap
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.travel-map {
+  height: 400px;
 }
 </style>
